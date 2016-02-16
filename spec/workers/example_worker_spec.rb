105aab9bafde6ff10c3764dd6f226bd35d40aa6b
Tanapat Sainak <tanapat@computerlogy.com>
2016-02-16 14:11:31 +0700

    remove example

--- spec/workers/example_worker_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe ExampleWorker do
-  it { is_expected.to be_processed_in :default }
-  # for more rspec-sidekiq matcher check at https://github.com/philostler/rspec-sidekiq
-  # gas.
-end
